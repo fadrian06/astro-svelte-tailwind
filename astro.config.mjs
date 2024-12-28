9dc7dd9e510cc3fa1f6a3827a7c277c938dc869d
fadrian06 <franyeradriansanchez@gmail.com>
2024-12-27 23:42:34 -0400

    Add default base matches CWD

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const __dirname = new URL(".", import.meta.url).pathname;
+const [, currentFolderName] = __dirname.split('/').reverse();
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: `/${currentFolderName}/`
+});
