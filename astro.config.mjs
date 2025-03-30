5f0d2838379290a9095c6709f51136d4a899cb31
fadrian06 <franyeradriansanchez@gmail.com>
2025-03-30 14:54:45 -0400

    npx astro add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
+import tailwind from '@astrojs/tailwind'
 
 const __dirname = new URL(".", import.meta.url).pathname;
 const [, currentFolderName] = __dirname.split('/').reverse();
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), tailwind()],
   base: `/${currentFolderName}/`
 });
